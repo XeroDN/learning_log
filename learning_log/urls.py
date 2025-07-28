d7083ac8d8384549df80c379ac137670bcf6a31c
XeroDN <rananiroj69@gmail.com>
2025-07-29 00:36:42 +0545

    intial commit

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('learning_logs.urls')),
+    path('', include(('learning_logs.urls', 'learning_logs'), namespace='learning_logs')),
 ]
